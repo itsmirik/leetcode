291f8774401aebb83cfd57ad260cbde908c4af3c
Mirsaid Akhmedov <akhmedovmirik@gmail.com>
2025-01-21 08:19:41 +0500

    3194. Minimum Average of Smallest and Largest Elements v2

--- problems/TwoPointers/3194.py
+++ problems/TwoPointers/3194.py
@@ -3,8 +3,23 @@ from typing import List
 
 class Solution:
     def minimumAverage(self, nums: List[int]) -> float:
-        nums.sort()
-        return min((nums[i] + nums[~i]) / 2 for i in range(len(nums) // 2))
+        left, right = 0, len(nums) - 1
+        sorted_nums = sorted(nums)
+        min_avg = (sorted_nums[left] + sorted_nums[right]) / 2
+
+        while left < right:
+            calc = (sorted_nums[left] + sorted_nums[right]) / 2
+
+            if calc < min_avg:
+                min_avg = calc
+
+            left += 1
+            right -= 1
+
+        return min_avg
+
 
 if __name__ == '__main__':
     print(Solution().minimumAverage([7, 8, 3, 4, 15, 13, 4, 1]))
+    # print(Solution().minimumAverage([1, 9, 8, 3, 10, 5]))
+    # print(Solution().minimumAverage([1, 2, 3, 7, 8, 9]))
