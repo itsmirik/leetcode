a9342cf210ac5ea5241296f581ea65b1373952e8
Mirsaid Akhmedov <akhmedovmirik@gmail.com>
2025-01-23 08:12:54 +0500

    2108. Find First Palindromic String in the Array v2

--- problems/TwoPointers/2108.py
+++ problems/TwoPointers/2108.py
@@ -3,26 +3,17 @@ from typing import List
 
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
+        palindrome = ''
+
         for word in words:
-            if self.is_palindrome(word):
-                return word
+            if word == word[::-1]:
+                palindrome = word
+                break
 
-        return ''
-
-    def is_palindrome(self, word: str) -> bool:
-        n = len(word)
-        if n == 1:
-            return True
-
-        left, right = 0, n - 1
-        while left < right:
-            if word[left] != word[right]:
-                return False
-
-            left += 1
-            right -= 1
-
-        return True
+        if palindrome:
+            return palindrome
+        else:
+            return ''
 
 
 if __name__ == '__main__':
